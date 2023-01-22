ae0469bbe7ad5bb85b66305fb7e277c1dcc1e720
Sahil Chahal <dev.sahilchahal@gmail.com>
2023-01-22 17:57:35 +0530

    Get Countries static fumnction

--- src/Countries.php
+++ src/Countries.php
@@ -2,5 +2,10 @@
 namespace Chahal26\CountriesDataPhp;
 
 Class Countries {
-    
+
+    public static function getCountries(){
+        $getData = file_get_contents('../data/countries.json');
+        $data = json_decode($getData);
+       return collect($data);
+    }
 }
