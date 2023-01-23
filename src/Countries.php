c6f014bd5c85506b5e4d41e5ab9713c59ae82174
Sahil Chahal <dev.sahilchahal@gmail.com>
2023-01-23 17:41:11 +0530

    added file_path as seperate property

--- src/Countries.php
+++ src/Countries.php
@@ -3,8 +3,17 @@ namespace Chahal26\CountriesDataPhp;
 
 Class Countries {
 
-    public static function getCountries(){
-        $getData = file_get_contents('../data/countries.json');
+    /**
+     * Path to json file
+     */
+    public static $file_path = '../data/countries.json';
+
+    /**
+     * Get all data
+     */
+    public static function getCountries()
+    {
+        $getData = file_get_contents(static::$file_path);
         $data = json_decode($getData);
        return collect($data);
     }
