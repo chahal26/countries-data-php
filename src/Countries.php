f6959ca1a35ac00933698ca5ae266b922f40297b
Sahil Chahal <dev.sahilchahal@gmail.com>
2023-01-24 17:39:23 +0530

    Updated getCountries method

--- src/Countries.php
+++ src/Countries.php
@@ -6,6 +6,7 @@ Class Countries {
     public static function getCountries(){
         $getData = file_get_contents('../data/countries.json');
         $data = json_decode($getData);
-       return collect($data);
+        
+        return $data;
     }
 }
